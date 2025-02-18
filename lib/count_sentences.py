0575c28f97d85b051b74c308efc358ea30683d38
Martha Wangui Mwangi <marthawm7252@gmail.com>
2025-02-18 19:10:19 +0300

    python-p3-oo-counting-sentences-lab

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,4 +1,34 @@
 #!/usr/bin/env python3
 
+import re
 class MyString:
-  pass
+  def __init__(self, value = ''):
+        self._value = value
+        
+  def get_value(self):
+      return self._value
+
+  def set_value(self, value):
+      if (type(value) == str):
+          print(f"Setting value to {value}")
+          self._value = value
+      else:
+          print("The value must be a string.")
+
+  value = property(get_value, set_value)
+
+  def is_sentence(self):
+        return self._value.endswith(".")
+  
+  #The endswith() method is a built-in string method in Python that is used to check whether a string ends with a specified suffix or sequence of characters.
+  def is_question(self):
+        return self._value.endswith("?")
+  
+  def is_exclamation(self):
+        return self._value.endswith("!")
+  
+  def count_sentences(self):
+        if self.value == '':
+            return 0
+        else:
+            return len(re.split(r'[!\?\.](?= )', self.value))
